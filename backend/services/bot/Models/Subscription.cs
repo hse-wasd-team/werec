ab9453cb759196a9e0ef14c8f1926861d0d05dbf
Yusupova Ramilya <sr.eagleowl@gmail.com>
2022-11-27 16:40:03 +0300

    add bot script

---
+++ backend/services/bot/Models/Subscription.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.Serialization;
+
+namespace WeRecWebApp.Models
+{
+    public class Subscription
+    {
+        [DataMember(Name = "id")]
+        public string Id { get; set; }
+
+        [DataMember(Name = "feed")]
+        public string FeedId { get; set; }
+
+        [DataMember(Name = "user")]
+        public string User { get; set; }
+    }
+}
